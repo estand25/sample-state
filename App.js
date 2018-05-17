c4931c935a57b04a05fa166dbad07289c283c878
Standley Eugene <estand25@gmail.com>
2018-05-16 21:12:50 -0400

    Add new component Enlarge

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Blink from './src/components/Blink';
+// import Blink from './src/components/Blink';
+import { Enlarge } from './src/components/Enlarge';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Blink text='I love to blink' />
-      </View>
+      // <View style={styles.container}>
+      //   <Blink text='I love to blink' />
+      // </View>
+      // <View>
+        <Enlarge />
+      // </View>
     );
   }
 }
