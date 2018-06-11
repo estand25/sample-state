9f3b331b4b9516358f7ced2033470eddd8558007
Standley Eugene <estand25@gmail.com>
2018-06-10 21:23:33 -0400

    Add ImageRecognition and result const

--- src/config/rn-cli.config.js
+++ src/config/rn-cli.config.js
@@ -3,3 +3,12 @@ module.exports = {
     return ['pb', 'txt']
   }
 }
+
+const tfImageRecognition = new tfImageRecognition({
+  model: require(''),
+  labels: require(''),
+});
+
+const results = await ifImageRecognition.recognize({
+  image: require(''),
+})
